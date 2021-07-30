5d22b061f1785ef879fb3d8cf33c3816b21ab92d
EpicGiant27 <82335462+EpicGiant27@users.noreply.github.com>
2021-07-30 19:03:37 +0100

    Add files via upload

---
+++ index.bat
@@ -0,0 +1,15 @@
+@echo off
+title Spec Checker
+echo -Sizes In Bytes.-
+echo.
+echo -===== CPU =====-
+wmic cpu get name, maxclockspeed, currentclockspeed
+echo -===== GPU =====-
+wmic path win32_VideoController get name
+echo -= Motherboard==-
+wmic baseboard get product,Manufacturer.
+echo -===== RAM =====-
+wmic MEMORYCHIP get BankLabel, DeviceLocator, Capacity, Speed
+echo -==== DRIVE ====-
+wmic diskdrive get model,serialNumber,size,mediaType
+pause
